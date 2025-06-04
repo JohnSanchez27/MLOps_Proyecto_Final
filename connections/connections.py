29730dc6277f16fa023c55e65d8ec62e3c3d0ac4
JohnSanchez27 <jrsanchezr8906@gmail.com>
2025-06-04 06:14:55 -0500

    mergin

--- connections/connections.py
+++ connections/connections.py
@@ -3,13 +3,13 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.exc import OperationalError
 
 PASSWORD = 'Compag*87'
-HOST = '10.43.101.200:31306'  # Este es el nombre del servicio Docker, no 'localhost' mysql
+HOST = 'mysql'  # Este es el nombre del servicio Docker, no 'localhost' mysql
 
 # Esperar hasta que MySQL esté disponible
 def esperar_mysql():
     while True:
         try:
-            engine = create_engine(f'mysql+pymysql://root:{PASSWORD}@{HOST}')
+            engine = create_engine(f'mysql+pymysql://root:{PASSWORD}@{HOST}:3306')
             with engine.connect() as conn:
                 conn.execute(text("SELECT 1"))
             break
@@ -21,14 +21,14 @@ esperar_mysql()
 
 
 # Crear bases si no existen
-engine_server = create_engine(f'mysql+pymysql://root:{PASSWORD}@{HOST}')
+engine_server = create_engine(f'mysql+pymysql://root:{PASSWORD}@{HOST}:3306')
 with engine_server.connect() as conn:
     conn.execute(text("CREATE DATABASE IF NOT EXISTS RAW_DATA"))
     conn.execute(text("CREATE DATABASE IF NOT EXISTS CLEAN_DATA"))
 
 # Conexiones
-engine_raw_data = create_engine(f'mysql+pymysql://root:{PASSWORD}@{HOST}/RAW_DATA')
-engine_clean_data = create_engine(f'mysql+pymysql://root:{PASSWORD}@{HOST}/CLEAN_DATA')
+engine_raw_data = create_engine(f'mysql+pymysql://root:{PASSWORD}@{HOST}:3306/RAW_DATA')
+engine_clean_data = create_engine(f'mysql+pymysql://root:{PASSWORD}@{HOST}:3306/CLEAN_DATA')
 
 connectionsdb = [engine_raw_data, engine_clean_data]
 
